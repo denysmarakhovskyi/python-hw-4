478514de19b861d5260d5bac807d319a85bceca8
denysmarakhovskyi <denysmarakhovskyi@gmail.com>
2022-07-25 18:42:27 +0200

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,6 +1,9 @@
 import sys
 from typing import List
-def TwoSum(nums: List[int], target: int) -> List[int]:
+def twoSum(nums: List[int], target: int) -> List[int]:
     print(nums, target)
 
-TwoSum([1, 2, 3], 4)
+twoSum([1, 2, 3], 4)        # [0, 2]
+twoSum([2, 7, 11, 15], 9)   # [0, 1]
+twoSum([3, 2, 4], 6)        # [1, 2]
+twoSum([3, 3], 6)           # [0, 1]
