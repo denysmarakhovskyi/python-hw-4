66c45a854c2a1151e6d2255c03bb2a350c46d476
denysmarakhovskyi <denysmarakhovskyi@gmail.com>
2022-08-01 16:21:34 +0200

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,9 +1,54 @@
 import sys
+from collections import defaultdict
 from typing import List
-def twoSum(nums: List[int], target: int) -> List[int]:
-    print(nums, target)
 
-twoSum([1, 2, 3], 4)        # [0, 2]
-twoSum([2, 7, 11, 15], 9)   # [0, 1]
-twoSum([3, 2, 4], 6)        # [1, 2]
-twoSum([3, 3], 6)           # [0, 1]
+
+# Returns the indices of two numbers that add up to the target
+def two_sums(nums, target):
+
+    lookup = defaultdict(list)
+    for i, num in enumerate(nums):
+        needed = target - num
+        if needed in lookup:
+            return [lookup[needed][0], i]
+        lookup[num].append(i)
+
+    return None
+
+
+print(two_sums([1, 2, 3], 4))  # [0, 2]
+print(two_sums([2, 7, 11, 15], 9))  # [0, 1]
+
+
+def twoSum(nums, target):
+    d = {}
+    for i in range(len(nums)):
+        if target - nums[i] in d:
+            return [d[target - nums[i]], i]
+        else:
+            d[nums[i]] = i
+
+
+print(twoSum([3, 2, 4], 6))  # [1, 2]
+
+
+class Solution(object):
+    def TooSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        required = {}
+        for i in range(len(nums)):
+            if target - nums[i] in required:
+                return [required[target - nums[i]], i]
+            else:
+                required[nums[i]] = i
+
+
+input_list = [3, 3]
+ob1 = Solution()
+print(ob1.TooSum(input_list, 6))  # [0, 1]
+
+
